4939b0e9d7487a799422329b832e59199b4d251e
amir <serogenera@proton.me>
2025-03-21 03:15:57 +0300

    Add gradient background to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <Header />
+      <div className="bg-gradient-to-r from-zinc-500 to-violet-400 animate-gradient bg-[size:200%_200%] min-h-screen pt-20">
+        {/* Ваш контент здесь */}
+      </div>
     </>
   )
 }

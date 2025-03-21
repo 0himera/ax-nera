67fe3da09d2b45e1a4887066b85f47e7ea96c460
amir <serogenera@proton.me>
2025-03-21 03:16:04 +0300

    Update Header component styles for improved responsiveness and visual appeal. Adjusted layout properties, added backdrop blur, and modified text sizes for better readability.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,19 @@ import Barcode from './BarcodeLogo';
 
 const Header = () => {
   return (
-     <div className="h-[50px] w-[960px] mt-4 mx-auto rounded-full flex items-center justify-around bg-black">
+     <div className="fixed top-0 left-0 right-0 z-50 h-[50px] max-w-[960px] 
+                     mt-4 mx-[5%] lg:mx-auto rounded-full flex 
+                     items-center justify-around bg-black/10 
+                     backdrop-blur-lg border border-zinc-600/20 shadow-lg
+                     text-white">
           <div className="logo">
-               <h1 className="text-white text-xl font-bold">
+               <h1 className="text-white text-lg lg:text-xl  font-bold">
                     nera *
                </h1>
                <Barcode />
           </div>
-          <div className="text-white">
-               <ul className="flex items-center gap-4">
+          <div>
+               <ul className="flex items-center gap-2 text-sm">
                     <li>
                          <a href="#">Home</a>
                     </li>
